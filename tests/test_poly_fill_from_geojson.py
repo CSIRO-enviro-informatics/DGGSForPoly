85e4e0ca6b27033d8aacfff8bce7668ed32c692e
Ross Petridis <71374438+Lazazz@users.noreply.github.com>
2022-01-10 16:48:30 +1100

    adding tests

--- tests/test_poly_fill_from_geojson.py
+++ tests/test_poly_fill_from_geojson.py
@@ -1,5 +1,5 @@
 from DGGSForPoly import poly_fill
-from get_data import get_static_test_data_Black_Mountain, get_poly_fill_ex2_answer
+from get_data import get_static_test_data_Black_Mountain, get_polly_fill_from_geojson_answer_ex2
 from DGGSForPoly.cell_area import area_of_cells
 from math import isclose
 
@@ -8,7 +8,7 @@ def test_poly_fill_from_geojson_ex1():
     geojson_obj = get_static_test_data_Black_Mountain()
     list_of_lists_of_dggs_cells = poly_fill.poly_fill_from_geojson(geojson_obj=geojson_obj, max_res=10, fill_strategy='poly_fully_covered_by_cells')
 
-    cells_answers = get_poly_fill_ex2_answer()
+    cells_answers = get_polly_fill_from_geojson_answer_ex2()
     area_answers = [28484442.697446138, 805472.6726784256, 8249992.829251752]
     
     for i,cell_list in enumerate(list_of_lists_of_dggs_cells):
